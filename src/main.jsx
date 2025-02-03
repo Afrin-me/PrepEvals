9d13cf7b6a080979321eb6c0e6ee6793240a1f52
Afrin-me <shaikhafrin1125@gmail.com>
2025-02-03 23:46:36 +0530

    adding secong commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // import ProtectedRoute from "./context/ProtectedRoute.jsx";
 import AuthContextProvider from "./context/AuthContextProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthContextProvider>
         {/* ✅ AuthContextProvider wraps everything first */}
 
@@ -17,6 +17,8 @@ createRoot(document.getElementById("root")).render(
         <App />
         {/* </ProtectedRoute> */}
       </AuthContextProvider>
-    </BrowserRouter>
+    </HashRouter>
+
+    {/* </BrowserRouter> */}
   </StrictMode>
 );
